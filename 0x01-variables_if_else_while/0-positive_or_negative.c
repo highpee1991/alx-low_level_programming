5370e60d0b89c2a09ce968425f5c7e31f8a9b77b
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 11:23:51 -0700

    display random number and indicate of is negative, zero or postive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* header location */
 
-/* code body */
+/**
+ * main - Entry point of the program
+ *
+ * This function generates a random number and determines if it is positive,
+ * negative, or zero.
+ * It then displays the number and the result of the condition.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(NULL));
 	n = rand() - RAND_MAX / 2;
+	printf("%d", n); /* declare the value before checking the condition */
+
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("is positive\n");
 	} else if (n == 0)
 	{
-		printf("is zero");
+		printf("is zero\n");
 	} else
 	{
-		printf("is negative");
+		printf("is negative\n");
 	};
 	return (0);
 }
