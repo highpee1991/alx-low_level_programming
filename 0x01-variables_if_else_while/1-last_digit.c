e0ef83aec579797c98d2dcea4d1d1b1f948b2293
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 18:25:04 -0700

    Assigns a random number to the variable n and prints its last digit.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,30 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-/** assign a random number to the variable n.
+/**
+ * main - Entry point of the program
  *
- *  each time it is executed. 
- *  Complete the source code in order to print t
- *  he last digit of the number stored in the 
- *  variable n
- **/
+ * This function generates a random number and determines if it is positive,
+ * negative, or zero.
+ * It then displays the number and the result of the condition.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-	int n, lastdigit;
+	int n, lastdigit, result;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = abs(n) % 10;
-	printf("Last digit of %d is %d ",n, lastdigit);
-	if (lastdigit > 5)
+	result = n < 0 ? -lastdigit : lastdigit;
+	printf("Last digit of %d is %d ", n, result);
+
+	if (result > 5)
 	{
 		printf("and is greater than 5\n");
-	} else if (lastdigit == 0) 
+	} else if (result == 0)
 	{
 		printf("and is 0\n");
-	} else 
+	} else
 	{
 		printf("and is less than 6 and not 0\n");
 	};
+	/* print zero if one of the condtion is succesful */
 	return (0);
 }
