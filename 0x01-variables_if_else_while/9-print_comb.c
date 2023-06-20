c9615a3a7d8988249af67e36acdca732445c53df
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 20:59:07 -0700

    digut with comma seprated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function generates digit separated by comma,
+ * using putchar only and not more than 4 times .
+ * and print the result.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num != 9) 
+		{
+			putchar(',');
+			putchar(' ');
+		};
+	};
+	putchar('\n');
+	return (0);
+}
