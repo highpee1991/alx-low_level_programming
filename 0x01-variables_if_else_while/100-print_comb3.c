f9f03bf6d0f58cfcef1e341685d4b78ca3a18b21
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 21:34:50 -0700

    2 number

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,15 +16,18 @@ int main(void)
 
 	for (dig1 = 0; dig1 <= 8; dig1++)
 	{
-		for (dig2 = 1; dig2 <= 9; dig2++)
+		for (dig2 = dig1 + 1; dig2 <= 9; dig2++)
 		{
 			putchar(dig1 + '0');
 			putchar(dig2 + '0');
 
 			if (dig1 != 8 || dig2 != 9)
 			{
-				putchar(',');
-				putchar(' ');
+				if (!(dig1 == 0 && dig2 == 1))
+				{
+					putchar(',');
+					putchar(' ');
+				};
 			};
 		};
 	};
