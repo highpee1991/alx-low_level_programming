7ae25ca63969a173fcb67b981b911c67bf313723
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 21:19:58 -0700

    2 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function generates 2 digit separated by comma,
+ * using putchar only and not more than 4 times .
+ * and print the result.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 <= 8; dig1++)
+	{
+		for (dig2 = 1; dig2 <= 9; dig2++)
+		{
+			putchar(dig1 + '0');
+			putchar(dig2 + '0');
+
+			if (dig1 != 8 || dig2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			};
+		};
+	};
+	putchar('\n');
+	return (0);
+}
