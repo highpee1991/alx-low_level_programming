5d8293be25c9fab1fc71a5f124dcc29eac2abaaa
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 18:41:33 -0700

    print letter a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** main - code start
+ *
+ * write letter a-z in lowercase
+ * return - 0 success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	};
+	
+	putchar('\n');
+	return (0);
+}
