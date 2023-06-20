dc7ba284f452344f8dccd1d54721c49a2bbf9e15
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-19 22:19:50 -0700

    2 digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,9 +4,9 @@
 /**
  * main - Entry point of the program
  *
- * This function generates 2 digit separated by comma,
- * using putchar only and not more than 4 times .
- * and print the result.
+ * This function generates 2 digits separated by a comma,
+ * using putchar only and not more than 4 times,
+ * and prints the result.
  *
  * Return: 0 on success
  */
@@ -20,17 +20,13 @@ int main(void)
 		{
 			putchar(dig1 + '0');
 			putchar(dig2 + '0');
-
 			if (dig1 != 8 || dig2 != 9)
 			{
-				if (!(dig1 == 0 && dig2 == 1))
-				{
-					putchar(',');
-					putchar(' ');
-				};
-			};
-		};
-	};
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
