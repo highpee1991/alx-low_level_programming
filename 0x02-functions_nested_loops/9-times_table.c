dc8546b6b17b59dbee9ef3a64c68b9776673df9d
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-21 18:18:56 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,21 +15,21 @@ void times_table(void)
 		for (column = 0; column < 10; column++)
 		{
 			result = row * column;
-
-			if (result <= 9)
+			if (column > 0)
 			{
+				_putchar(',');
+			}
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + result);
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar('0' + (result / 10));
 				_putchar('0' + (result % 10));
-			}
-
-			if (column < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar(10);
