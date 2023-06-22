56a62c0a70da4ad30313fb330aa81bfbb85acd75
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-22 14:34:06 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - Checks if a number is positive or negative.
+ * @i: The number to be checked.
+ * This function takes an integer as input and prints whether it is
+ * positive, negative, or zero.
+*/
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
