c97f742e42cf68af078d2cba8fe5fe2d17aa678c
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-22 04:44:51 -0700

    sum of numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function generates 2 of 2 digit separated by comma,
+ * using putchar only and not more than 4 times .
+ * and print the result.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int limit = 1024;
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i
+		}
+	}	
+	printf("%d", sum);
+	return (0);
+}
