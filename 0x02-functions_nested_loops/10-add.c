7372f480021fed917201dc740ec154e958298bd5
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-21 18:54:09 -0700

    add num

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Adds two numbers
+ *
+ * @n: First number to be added
+ * @m: Second number to be added
+ *
+ * Description: This function adds two numbers and returns the result.
+ * The result is calculated using only the
+ *
+ * _putchar function and no standard library.
+ * Return: The sum of n and m
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+
+	return (result);
+}
