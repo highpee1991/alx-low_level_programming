c9e98359c485e74ea39563e6d7bb0df0007878d1
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-23 16:56:51 -0700

    multiply num

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - hold the valude that need to be multiply.
+ *
+ * @a: The the value to multiply.
+ * @b: the value to multiply with value a.
+ *
+ * Return: result success.
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
