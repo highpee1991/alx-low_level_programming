504c735eaa8e535ea1eb69366f659e8d51c777bf
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-27 19:27:26 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,20 @@
  */
 void rev_string(char *s)
 {
-        int len = 0;
+	int len = 0;
 	int i;
 	char j;
-	
+
 	if (s == NULL)
 		return;
 
-        while (s[len] != '\0')
-                len++;
+	while (s[len] != '\0')
+		len++;
 
-        for (i = 0; i < len / 2; i++)
-        {
-                j = s[i];
+	for (i = 0; i < len / 2; i++)
+	{
+		j = s[i];
 		s[i] = s[len - 1 - i];
 		s[len - 1 - i] = j;
-        }
+	}
 }
