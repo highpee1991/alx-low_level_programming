cba52e866da765a40da81718f48efd871bfe4ab5
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-06-27 20:30:04 -0700

    print every other number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j = 0;
 
 	if (str == NULL)
 		return;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[j] != '\0')
+		j++;
+
+	for (i = 0; i < j; i += 2)
 	{
 		if (isprint((unsigned char)str[i]))
 			putchar(str[i]);
