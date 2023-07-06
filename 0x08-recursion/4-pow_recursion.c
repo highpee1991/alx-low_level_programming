09e4fc349f71b88e9cabc8fcece89be802640c28
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-05 18:33:07 -0700

    power of value

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <math.h>
+
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y.
+ * @x: The base number.
+ * @y: The exponent.
+ *
+ * Return: The value of x raised to the power of y.
+ *
+ * Description: This function calculates the value of x raised to the power of y
+ * using recursive calls. If y is negative, it returns -1. Otherwise, it uses the
+ * pow function from the math library to calculate the result and returns it.
+ */
+int _pow_recursion(int x, int y)
+{
+	double res = pow(x, y);
+
+	if (y < 0)
+		return (-1);
+
+	return (res);
+}
