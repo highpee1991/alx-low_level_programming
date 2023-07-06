3751d4910cbc3e9a0fad56ef09f025eaa957639f
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-06 15:17:11 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n < 2)
+		return (0);
+
+	for (i = 2; n / i >= i; i++)
+	{
+		if (n % i == 0)
+		return (0);
+	}
+	return (1);
+}
