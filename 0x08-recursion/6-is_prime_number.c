6d616d6848970b87f2f5ca8f557eec45ed2ce93d
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-06 20:37:35 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,27 +8,28 @@
  */
 int is_prime_number(int n)
 {
+	int divisor = n / 2;
+
 	if (n < 2)
 		return (0);
-    return is_divisible_recursive(n, 2, n/2);
+	return (is_divisible_recursive(n, 2, divisor));
 }
-
 /**
- * is_divisible_recursive - Helper function to check if a number is divisible by a divisor recursively.
+ * is_divisible_recursive - Helper function to check
+ * if a number is divisible by a divisor recursively.
  * @n: The number to check.
  * @divisor: The divisor to check.
- * @limit: The upper limit of divisors to check.
+ * @lim: The upper limit of divisors to check.
  *
  * Return: 1 if divisible, 0 otherwise.
  */
 int is_divisible_recursive(int n, int divisor, int lim)
 {
-    if (n % divisor == 0 && divisor < lim)
-        return (0);
+	if (n % divisor == 0 && divisor < lim)
+		return (0);
 
-    if (divisor > lim)
-        return (1);
+	if (divisor > lim)
+		return (1);
 
-    return is_divisible_recursive(n, divisor + 1, lim);
+	return (is_divisible_recursive(n, divisor + 1, lim));
 }
-
