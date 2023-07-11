bea3378369029ef730320e0c9f5d810938d3d375
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-11 14:28:32 -0700

    display all aguement

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
