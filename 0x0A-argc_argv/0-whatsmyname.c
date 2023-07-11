77daa58179fc055fa514a09a25f06ada71da61c7
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-11 12:12:22 -0700

    display name of first agument argv[]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
