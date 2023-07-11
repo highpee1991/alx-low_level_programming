da5e1b31a6da1b8996094259a9f656db13fdb3ef
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-11 15:18:27 -0700

    multiply 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int i;
 	int mul = 1;
 
-	if (argc > 1)
+	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 	else
 	{
 		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
