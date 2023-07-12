1927411112bff92f26f5fe94fe58d13334658fd3
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-11 17:04:45 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,30 +12,30 @@
  */
 int main(int argc, char *argv[])
 {
-        int i;
-        int sum = 0;
+	int i;
+	int sum = 0;
 
-        if (argc > 2)
-        {
-                for (i = 1; i < argc; i++)
-                {
-                        int digit = atoi(argv[i]);
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int digit = atoi(argv[i]);
 
-                        if (isdigit(*argv[i]) && digit >= 0)
-                        {
-                                sum += digit;
-                        }
-                        else
-                        {
-                                printf("Error\n");
-                                return (1);
-                        }
-                }
-                printf("%d\n", sum);
-        }
-        else
-        {
-                printf("0\n");
-        }
-        return (0);
+			if (isdigit(*argv[i]) && digit >= 0)
+			{
+				sum += digit;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
 }
