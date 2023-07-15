40b335fac5a47f9687b2702b84602c7313e0112d
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-15 16:22:35 -0700

    reallocate memory using malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 /**
@@ -26,8 +27,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *_pointer, *pt;
-	unsigned int pos;
+	void *pt;
+	unsigned int size_copy;
 
 	if (old_size == new_size)
 		return (ptr);
@@ -38,32 +39,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (ptr == NULL && old_size != 0)
-		new_size = old_size;
+	if (ptr == NULL)
+		return malloc(new_size);
 
-	pt = (char *)ptr;
-	_pointer = malloc(sizeof(char) * new_size);
-
-	if (_pointer == NULL)
-		return (NULL);
+	pt = malloc(new_size);
 
 	if (pt == NULL)
-		return (_pointer);
+		return (NULL);
 
-	if (new_size > old_size)
-	{
-		for (pos = 0; pos <= old_size; pos++)
-		{
-			_pointer[pos] = pt[pos];
-		}
-	}
-	else
-	{
-		for (pos = 0; pos < new_size; pos++)
-		{
-			_pointer[pos] = pt[pos];
-		}
-	}
+	size_copy = (new_size < old_size ? new_size : old_size);
+	memcpy(pt, ptr, size_copy);
 	free(ptr);
-	return (_pointer);
+
+	return (pt);
 }
