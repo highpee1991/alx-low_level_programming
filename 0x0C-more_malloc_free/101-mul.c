9ada5f678dacbc05eeb47607b5ade65b3ac8148c
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-15 17:46:54 -0700

    mult 2 numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,19 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * mul - Multiplies two positive numbers.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: This program multiplies two positive numbers passed as
+ * command-line arguments in base 10. It prints the result of the
+ * multiplication, followed by a new line. If the number of arguments is
+ * incorrect or the arguments are not composed of digits, it prints "Error",
+ * followed by a new line, and exits with a status of 98.
+ */
 int mul(int argc, char *argv[])
 {
 	int num1, num2;
+
 	if (argc != 3)
-        {
-                printf("Error\n");
-                exit(98);
-        }
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	num1 = atoi(argv[1]);
-        num2 = atoi(argv[2]);
+	num2 = atoi(argv[2]);
 
-	if (!isdigit((unsigned char)argv[1][0]) || !isdigit((unsigned char)argv[2][0]))
+	if (!isdigit((unsigned char)argv[1][0]) ||
+	!isdigit((unsigned char)argv[2][0]))
 	{
 		printf("Error\n");
 		exit(98);
@@ -25,8 +40,17 @@ int mul(int argc, char *argv[])
 
 	return (0);
 }
-
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: The main function calls the mul function to multiply two
+ * positive numbers passed as command-line arguments.
+ */
 int main(int argc, char *argv[])
 {
-	return mul(argc, argv);
+	return (mul(argc, argv));
 }
