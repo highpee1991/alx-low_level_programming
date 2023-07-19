9eae6675eff371324f41918efacfda6568616e2c
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-19 20:14:06 +0000

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
+/**
+ * new_dog - Creates a new dog with given attributes.
+ * @name: Pointer to the name of the dog.
+ * @age: Age of the dog.
+ * @owner: Pointer to the owner of the dog.
+ * Description:
+ * This function creates a new dog struct and initializes its attributes
+ * with the provided name, age, and owner. Memory is dynamically allocated
+ * for the new dog and its attributes (name and owner strings).
+ * The function returns a pointer to the new dog struct if successful,
+ * or NULL if memory allocation fails.
+ * Return: Pointer to the new dog struct on success, or NULL on failure.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int namelen, ownerlen, i;
-
-	namelen = 0;
-	while (name[namelen] != '\0')
-		namelen++;
-
-	ownerlen = 0;
-	while (owner[ownerlen] != '\0')
-		ownerlen++;
-
 
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(dog->name) * namelen);
-	if (dog == NULL)
-		return(NULL);	
+	namelen = strlen(name);
+
+	dog->name = malloc(sizeof(char) * (namelen + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
 	for (i = 0; i < namelen; i++)
 		dog->name[i] = name[i];
 
-	dog->owner = malloc(sizeof(dog->owner) * ownerlen);
-	if (dog == NULL)
+	ownerlen = strlen(name);
+
+	dog->owner = malloc(sizeof(char) * (ownerlen + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
+	}
+
 	for (i = 0; i < ownerlen; i++)
 		dog->owner[i] = owner[i];
 
