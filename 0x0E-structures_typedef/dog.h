fa9d22050bc08264daed9868b21dd7022f3f43b4
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-07-19 04:37:24 +0000

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,15 +12,25 @@
  *
  * Description: This structure defines the attributes of a dog, including its
  * name, owner, and age.
- *
  */
-/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif /* DOG_H */
