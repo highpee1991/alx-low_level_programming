b99afdda23e99715df1ebba2fcfd0b4fba698dba
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-08-02 06:10:24 +0000

    modify add node file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,10 +9,10 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, *current;
+	listint_t *node, *current;
 
 	node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (node == NULL)
 		return (NULL);
 	node->n = n;
 	node->next = NULL;
